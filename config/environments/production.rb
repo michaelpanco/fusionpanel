a06d4f30c11768f7352bb86bd7dca5025cd4dd47
Michael Panco <mhike_v2@yahoo.com>
2014-08-18 00:22:30 +0800

    Get works on Production Environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,20 +20,20 @@ Fusionpanel::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  #config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # config.assets.compile = true
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+ 	# config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  # config.assets.version = '1.0'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -77,4 +77,6 @@ Fusionpanel::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.precompile += %w( bootsrap_and_overrides.css )
+  config.action_mailer.delivery_method = :smtp
 end
