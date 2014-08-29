db35ce9a814a51addb2614d19568d41a2c679bae
Michael Panco <mhike_v2@yahoo.com>
2014-08-29 17:11:54 +0800

    Add contact message post url

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -1,6 +1,7 @@
 class FrontpageController < ApplicationController
 	
   layout false
+  skip_before_action :verify_authenticity_token, :only => [:sendmessage]
   
   def index
   	
@@ -21,6 +22,7 @@ class FrontpageController < ApplicationController
         @status_response = '200'
         request_url = request.original_url
         @active_page = URI(request_url).path.split('/').second
+        @authenticity_token = form_authenticity_token
         render page.template.location
       else
         @status_response = '404'
@@ -35,6 +37,7 @@ class FrontpageController < ApplicationController
         @content = page.content.html_safe
         @status_response = '200'
         @active_page = URI(request_url).path.split('/').second
+        @authenticity_token = form_authenticity_token
         render page.template.location
       else
         @status_response = '404'
@@ -61,5 +64,47 @@ class FrontpageController < ApplicationController
     return category_container[0..-2]
     
   end
+  
+  def sendmessage
+  	sleep 0.5
+		auth_token = params[:auth_token]
+  	name = params[:mpfname]
+  	email = params[:mpfemail]
+  	subject = params[:mpfsubject]
+  	message = params[:mpfmessage]
+  	
+  	response = Hash.new
+  	# form_authenticity_token
+  	if name != "" && email != "" && subject != "" && message != ""
+  		if auth_token == form_authenticity_token
+  			if valid_email? email
+					response["code"] = 100
+					response["message"] = "Success "
+					UserMailer.send_message(name, email, subject, message).deliver
+				else
+					response["code"] = 904
+					response["message"] = "Invalid Email Address "				
+				end
+				
+			else
+				response["code"] = 905
+				response["message"] = "Invalid Request form "
+			end
+  	else
+			response["code"] = 901
+			response["message"] = "Please complete all the fields."
+  	end
+  	
+		render json:response
+		
+  end
+  
+  private
 
+	def valid_email?(email)
+    valid_email = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    email.present? &&
+     (email =~ valid_email)
+  end
+  
 end
